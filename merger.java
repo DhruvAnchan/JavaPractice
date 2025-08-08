7dcc6f6d95bcf430a6ed8b0f00338d2779b2a6a6
DhruvAnchan <dhruvd.anchan@gmail.com>
2025-08-08 14:27:32 +0530

    mac aft

---
+++ merger.java
@@ -0,0 +1,13 @@
+public class merger {
+    public static void main(String[] args) {
+        int[] arr = {3,1,5,9,2,7};
+        int low = 0, high = arr.length, mid = (low+high)/2;
+        int[] x = merge(arr,low,mid,high);
+    }
+
+    private static int[] merge(int[] arr, int low, int high, int mid) {
+        
+        merge(arr)
+        return arr;
+    }
+}
